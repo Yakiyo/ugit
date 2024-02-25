e4c18f0f03a5be2fe8a7abe9ff5732a7ab0f7578
Yakiyo <yakiyo1206@gmail.com>
2024-02-25 17:34:59 +0000

    chore: impromptu commit

--- data/head.go
+++ data/head.go
@@ -3,15 +3,32 @@ package data
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/Yakiyo/ugit/utils"
+)
+
+var (
+	_PathHEAD = filepath.Join(GIT_DIR, "HEAD")
 )
 
 // set `id` as HEAD
 func SetHEAD(id string) error {
-	file, err := os.Create(filepath.Join(GIT_DIR, "HEAD"))
+	file, err := os.Create(_PathHEAD)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 	_, err = file.Write([]byte(id))
 	return err
+}
+
+// get commit from HEAD
+func GetHEAD() (string, error) {
+	// If HEAD doesn't exists, it means we don't have any commit
+	// for example its a newly created repository
+	if !utils.PathExists(_PathHEAD) {
+		return "", nil
+	}
+	content, err := os.ReadFile(_PathHEAD)
+	return string(content), err
 }
